b9e1992dcae4aa4726a468e87b6df69696b24026
IamHendy <puchhendy@gmail.com>
2022-09-08 14:29:53 -0700

    task 4 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,30 +3,15 @@
 
 
 /**
-
-* main - print the string in the put function
-
-*
-
-* Description: using the main function
-
-* this program prints "Programming is like building a multilingual puzzle
-
-* Return: 0
-
-*/
-
+ * main - Printing 
+ *
+ * Return: Always 0 (Successful) 
+ *
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+	return (0); 
 }
 
-
-
-
-
+	
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,28 +3,12 @@
 
 
 /**
-
-* main - print the string in the printf function
-
-*
-
-* Description: using the main function
-
-* this program prints "with proper grammar, but the outcome is a piece of art”
-
-* Return: 0
-
-*/
-
+ * main - Prints a text  
+ *
+ * Return: Always 0 (Success)  
+ */
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n"); 
+	  return (0); 
 }
-
-
-
