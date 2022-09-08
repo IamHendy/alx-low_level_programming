8470d7cf4c0fcab69c2fb90401404bcac926e680
IamHendy <puchhendy@gmail.com>
2022-09-08 11:28:55 -0700

    task 4

---
+++ 4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h
+
+/**
+ *
+ * *main -entry point
+ *
+ * *Return: 0 after rinting the function
+ *
+ * */
+
+int main(void)
+
+{"\""Programming is like building a multilingual puzzle");
+
+		return (0);
+
+}
