081eef8cc99e65aeaea8a2b5200a4b17294e405f
IamHendy <puchhendy@gmail.com>
2022-09-08 11:00:40 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>                                                                                                                                                                     
+
+                                                                                                                                                                                      
+
+/**                                          
+ * c programming  using put function
+ *
+ * return 0 value
+ *
+ */                                                                                                                                                                 
+
+int main (void)
+{puts("Programming is like building a multilingual puzzle")
+return (0);
+}
