e7877809ca7f205f6aaee7708b66a120b1c1a048
IamHendy <puchhendy@gmail.com>
2022-09-09 14:49:01 -0700

    task 2n3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<std10.h>
+
+/**
+ * main - lowercase alphabets
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
