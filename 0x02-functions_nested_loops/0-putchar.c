063c59391c7df2821735b535729d732136d9e2ce
IamHendy <puchhendy@gmail.com>
2022-09-13 09:59:24 -0700

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<main.h>
+#include <main.h>
 
 /**
  * main - prints putchar
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a' <= 'z'; j++)
+		{
+			putchar (j);
+		}
+		putchar ('\n');
+	}
