9dd53140298fd64ea7f20864f2c16f5eb7c3f513
IamHendy <puchhendy@gmail.com>
2022-09-16 02:09:28 -0700

    tsk 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,19 +9,16 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	printf("infinite loop incoming :(\n");
 
 	i = 0;
 
-
-	/* while (i < 10)*/
+	/*while (1 < 10)*/
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
 
-
-
-	printf("infinite loop avoided! \\o/n");
+	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
 }
