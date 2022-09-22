f0dd9ea565d8ddabc9ccd51fd517a165cce07dbb
IamHendy <puchhendy@gmail.com>
2022-09-22 13:28:11 -0700

    12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,6 +19,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
-char *string_toupper(char *);
+char *string_toupper(char );
 
 #endif /*MAIN_H*/
