5999e0e263abc9a202d7db2652023de00a4a8a7e
IamHendy <puchhendy@gmail.com>
2022-09-30 12:58:56 -0700

    task 0

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project entails information on thedifference between the Argc and ArgV function
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes ()
+ * int prototypes ()
+ * char prototypes ()
+ */
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
