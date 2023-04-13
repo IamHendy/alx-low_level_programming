8a040fb33500fc894bb5f0237d11caf0a3eae606
IamHendy <puchhendy@gmail.com>
2023-04-13 17:10:45 +0300

    task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,56 @@
 #include "search_algos.h"
 
+void print_array(int *array, size_t i, size_t size);
+
 /**
- * binary_search - searches for a value in a sorted array of integers,
- * using the Binary search algorithm.
- * @array: pointer to the first element of the array to search in.
- * @size: number of elements in array.
- * @value: value to search for.
- * Return: first index where value is located,
- * or -1 if value is not present or array is NULL.
- **/
+ * binary_search - binary search algorithm in a string
+ * @array: list of elements
+ * @size: size of the list
+ * @value: target to search
+ * Return: element index on success (-1) on fail
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, left, right;
+	size_t low = 0, high = size - 1, half = 0;
 
-	if (!array)
+	if (array == NULL || value == 0)
 		return (-1);
 
-	left = 0;
-	right = size - 1;
-	while (right >= left)
+	print_array(array, low, size);
+	while (low < high)
 	{
-		/* print array */
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-
-		i = left + (right - left) / 2;
-		if (array[i] < value)
-			left = i + 1;
-		else if (array[i] > value)
-			right = i - 1;
+		half = (low + high) / 2;
+		if (array[half] < value)
+		{
+			low = half + 1;
+			print_array(array, low, high + 1);
+		}
+		else if (array[half] > value)
+		{
+			high = half - 1;
+			print_array(array, low, high + 1);
+		}
 		else
-			return (i);
+			return (array[half]);
 	}
 	return (-1);
+}
+
+/**
+ * print_array - print all elements in an array
+ * @array: list of elements
+ * @i: index to init
+ * @size: size of the array
+ */
+void print_array(int *array, size_t i, size_t size)
+{
+	printf("Searching in array: ");
+	while (i < size)
+	{
+		if (i + 1 == size)
+			printf("%d ", array[i]);
+		else
+			printf("%d, ", array[i]);
+		i++;
+	} printf("\n");
 }
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -1,8 +1,1 @@
-# 0x1E. C - Search Algorithms
-
-## Learning Objectives
-
-- What is a search algorithm  
-- What is a linear search  
-- What is a binary search  
-- What is the best search algorithm to use depending on your needs  
+0x1E-search_algorithms
